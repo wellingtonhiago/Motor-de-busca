3eff5b47bc365a3a6191fc86d042bdd012ffe233
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-07-08 20:11:40 -0300

    Lendo arquivo de texto

--- src/main/kotlin/ExpandTheSearch.kt
+++ src/main/kotlin/ExpandTheSearch.kt
@@ -1,21 +1,37 @@
+import java.io.File
+
 fun main() {
-    println("Enter the number of books:")
-    val numberBooks = readln().toInt()
+    val fileName = "/Users/welli/Google Drive/Desenvolvedor/JetBrains Kotlin/Agenda.txt"
+    val books = File(fileName).readLines().toMutableList()
 
-    println("Enter all books:")
-    val books = MutableList(numberBooks) {
-        readln()
-    }
+    menu(books)
 
-    println("Enter the number of search queries:")
-    var numberSearch = readln().toInt()
 
-    repeat(numberSearch){
-        println("Enter data to search people:")
-        val searchWord = readln()
+}
 
-        if (searchWord.lowercase() in books.joinToString().lowercase()) for(book in books) {
-                if (searchWord.lowercase() in book.lowercase()) println(book)
-        } else println("No matching book found.")
-    }
+fun menu(books: MutableList<String>) {
+
+    var menu = true
+
+    do {
+        println("1. Find a person")
+        println("2. Print all people")
+        println("0. Exit")
+        when (readln()) {
+            "1" -> search(books = books)
+            "2" -> for (book in books) println(book)
+            "0" -> break
+            else -> continue
+        }
+    } while (menu)
+}
+
+fun search(books: MutableList<String>) {
+    println("Enter data to search people:")
+    val searchWord = readln()
+
+    if (searchWord.lowercase() in books.joinToString().lowercase())
+        for(book in books) {
+            if (searchWord.lowercase() in book.lowercase()) println(book)
+    } else println("No matching book found.")
 }
